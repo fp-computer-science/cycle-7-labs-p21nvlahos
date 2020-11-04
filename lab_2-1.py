406003120be448860812b66d5a327dc982e2c31c
Niko <p21nvlahos@fairfieldprep.org>
2020-11-04 09:16:36 -0500

    Add lab2-1

---
+++ lab_2-1.py
@@ -0,0 +1,13 @@
+# Author: NV 11/4/2020
+
+print('Fairfield' + ' Prep')
+
+print('-' * 20)
+
+print("I'm loving this short story I'm reading, \"The Fall of the House of Usher\"")
+
+previous_string = "I'm loving this short story I'm reading, \"The Fall of the House of Usher\""
+print(len(previous_string))
+
+my_string = "apple"
+print(my_string[3])
