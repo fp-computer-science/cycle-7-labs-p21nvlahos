4275e24477dae20f773307dc64692fe99bf350cc
Niko <p21nvlahos@fairfieldprep.org>
2020-11-06 13:16:11 -0500

    Add lab4-1 and lab4-2

---
+++ lab_4-1.py
@@ -0,0 +1,6 @@
+# Author: NV 11/6/2020
+fstring = "flibbeertigibbet"
+print(fstring.find('t'))
+
+sstring = "I wish, I wish, I was a fish."
+print(sstring.split(","))
---
+++ lab_4-2.py
@@ -0,0 +1,7 @@
+# Author: NV 11/6/2020
+
+person1 = input("what is your name ")
+person2 = input("What is the other persons name ")
+
+names = "Hello {0}. My name is {1}".format(person1, person2)
+print(names)
